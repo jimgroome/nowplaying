5d67f5a437e3fbdb26bc4a7514e82dbdd7802195
Jim Groome <james@jgroome.com>
2024-04-11 13:55:50 +0100

    Calculate play time differently

--- src/pages/api/tracks.ts
+++ src/pages/api/tracks.ts
@@ -17,11 +17,17 @@ const getTracks = async (req: NextApiRequest, res: NextApiResponse) => {
       (track: {
         artist: { "#text": string };
         name: string;
-        date: { "#text": string };
+        date: { "#text": string; uts: string };
       }) => {
         const artist = track.artist["#text"];
         const title = track.name;
         const time = track.date["#text"];
+        const uts = new Date(
+          parseInt(track.date.uts) * 1000
+        ).toLocaleTimeString();
+
+        console.log(uts);
+
         const spotify = `https://open.spotify.com/search/${encodeURIComponent(
           artist + " " + title
         )}`;
@@ -34,7 +40,7 @@ const getTracks = async (req: NextApiRequest, res: NextApiResponse) => {
         return {
           artist,
           title,
-          time,
+          time: uts,
           spotify,
           google,
           youtube,
@@ -49,49 +55,3 @@ const getTracks = async (req: NextApiRequest, res: NextApiResponse) => {
 };
 
 export default getTracks;
-
-// const axios = require("axios");
-
-// exports.tracks = async (event) => {
-//   return await axios
-//     .get(
-//       `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${
-//         event.queryStringParameters && event.queryStringParameters.station
-//           ? event.queryStringParameters.station
-//           : "bbc6music"
-//       }&limit=${
-//         event.queryStringParameters.tracks
-//       }&api_key=026b517c0d9e279b1b5215da1e165c3d&format=json`
-//     )
-//     .then((response) => {
-//       const tracksFromApi = response.data.recenttracks.track;
-//       const tracks = tracksFromApi.slice(1).map((track) => {
-//         const artist = track.artist["#text"];
-//         const title = track.name;
-//         const time = track.date["#text"];
-//         const spotify = `https://open.spotify.com/search/${encodeURIComponent(
-//           artist + " " + title
-//         )}`;
-//         const google = `https://www.google.com/search?q=${artist
-//           .split(" ")
-//           .join("+")}+${title.split(" ").join("+")}`;
-//         const youtube = `https://www.youtube.com/results?search_query=${artist
-//           .split(" ")
-//           .join("+")}+${title.split(" ").join("+")}`;
-//         return {
-//           artist,
-//           title,
-//           time,
-//           spotify,
-//           google,
-//           youtube,
-//         };
-//       });
-//       return {
-//         statusCode: 200,
-//         body: JSON.stringify(tracks),
-//         headers: { "Access-Control-Allow-Origin": "*" },
-//       };
-//     })
-//     .catch((e) => console.log(e));
-// };
